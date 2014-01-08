c8cd42609e96f9a686edc6af488e376364ed5da4
Robert Irelan <rirelan@gmail.com>
2014-01-07 22:40:43 -0800

    Add initial bones of init.el

---
+++ init.el
@@ -0,0 +1,11 @@
+;;; This file bootstraps the configuration, which is divided into
+;;; a number of other files.
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-local" containing personal settings
+;;----------------------------------------------------------------------------
+(when (file-exists-p (expand-file-name "init-local.el" user-emacs-directory))
+  (error "Please move init-local.el to ~/.emacs.d/lisp"))
+(require 'init-local nil t)
