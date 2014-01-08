9ab4d96f2424bd4c24484f1830919f2bd77c3b16
Robert Irelan <rirelan@gmail.com>
2014-01-07 22:45:09 -0800

    Add elpa package configuration

--- init.el
+++ init.el
@@ -3,6 +3,13 @@
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+; Load all the packages that are REQUIRE'd below
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
 ;;----------------------------------------------------------------------------
 ;; Allow users to provide an optional "init-local" containing personal settings
 ;;----------------------------------------------------------------------------
