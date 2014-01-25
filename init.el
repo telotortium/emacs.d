b960d79bc451b3eb3393d852afd8e69d54b7dfc6
Robert Irelan <rirelan@gmail.com>
2014-01-24 23:37:45 -0800

    Map C-c to function like C-g
    Helps Vim compatibility. Always have \C-c to get Emacs version if needed.

--- init.el
+++ init.el
@@ -31,6 +31,26 @@
 ; Kill current buffer but keep its frame around
 (define-key evil-normal-state-map "Q" 'kill-this-buffer)
 
+; C-c as general purpose escape key sequence (from EmacsWiki Evil page).
+(defun my-esc (prompt)
+  "Functionality for escaping generally.  Includes exiting Evil insert state and C-g binding. "
+  (cond
+   ;; If we're in one of the Evil states that defines [escape] key, return
+   ;; [escape] so as Key Lookup will use it.
+   ((or (evil-insert-state-p) (evil-normal-state-p) (evil-replace-state-p) (evil-visual-state-p)) [escape])
+   ;; This is the best way I could infer for now to have C-c work during
+   ;; evil-read-key.  Note: As long as I return [escape] in normal-state, I
+   ;; don't need this.  ((eq overriding-terminal-local-map evil-read-key-map)
+   ;; (keyboard-quit) (kbd ""))
+   (t (kbd "C-g"))))
+(define-key key-translation-map (kbd "C-c") 'my-esc)
+; Works around the fact that Evil uses read-event directly when in operator
+; state, which doesn't use the key-translation-map.
+(define-key evil-operator-state-map (kbd "C-c") 'keyboard-quit)
+; Not sure what behavior this changes, but might as well set it, seeing the
+; Elisp manual's documentation of it.
+(set-quit-char "C-c")
+
 
 ;; Make default encoding UTF-8 everywhere
 (setq current-language-environment "UTF-8")
