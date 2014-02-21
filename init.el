ea2d309f833d2e980187680d9884e17339b42808
Robert Irelan <rmirelan@google.com>
2014-02-21 00:58:47 -0800

    Add Javascript settings

--- init.el
+++ init.el
@@ -173,7 +173,18 @@
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
 
 ;; Javascript
-(add-to-list 'auto-mode-alist '("\\.javascript\\'" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.javascript\\'" . js-mode))
+
+;; Multi Web Mode - automatically switch to right major mode in HTML files
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
+
 
 ;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
