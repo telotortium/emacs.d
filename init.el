250e0c15a090ee45fd5f71d848838b2d3c2e43c6
Robert Irelan <rirelan@gmail.com>
2015-08-24 11:47:28 -0700

    Move MELPA URL to new top-level domain and enable HTTPS
    See https://github.com/milkypostman/melpa/issues/2109.

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (package-initialize)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") 'append)
 
 (require 'cl-lib)
