f6753870772a16ccfc3640509a1322ba4cb0f90e
Robert Irelan <rirelan@gmail.com>
2017-06-21 19:10:59 -0700

    Implement "d" and "D" org agenda commands (high-priority task isolation)
    See
    https://blog.aaronbieber.com/2016/09/24/an-agenda-for-life-with-org-mode.html.

--- init.el
+++ init.el
@@ -1058,6 +1058,45 @@ of occur. The original buffer is not modified.
                   (org-agenda-archives-mode t) ; include archive files
                 )))
 
+;;; https://blog.aaronbieber.com/2016/09/24/an-agenda-for-life-with-org-mode.html
+(add-to-list 'org-agenda-custom-commands
+             '("d" "Daily agenda with high-priority items isolated (slow)"
+               ((tags "PRIORITY=\"A\""
+                      ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                       (org-agenda-overriding-header "High-priority unfinished tasks:")))
+                (agenda "" ((org-agenda-ndays 1)
+                            (org-agenda-overriding-header "Schedule"))))
+               ((org-agenda-compact-blocks t))))
+(add-to-list 'org-agenda-custom-commands
+             '("D" "Like d but include all TODOs (slow)"
+               ((tags "PRIORITY=\"A\""
+                      ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                       (org-agenda-overriding-header "High-priority unfinished tasks:")))
+                (agenda "" ((org-agenda-ndays 1)
+                            (org-agenda-overriding-header "Schedule")))
+                (alltodo ""
+                         ((org-agenda-skip-function '(or (air-org-skip-subtree-if-habit)
+                                                         (air-org-skip-subtree-if-priority ?A)
+                                                         (org-agenda-skip-if nil '(scheduled deadline))))
+                          (org-agenda-overriding-header "ALL normal priority tasks:"))))
+               ((org-agenda-compact-blocks t))))
+(defun air-org-skip-subtree-if-priority (priority)
+  "Skip an agenda subtree if it has a priority of PRIORITY.
+
+PRIORITY may be one of the characters ?A, ?B, or ?C."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+        (pri-value (* 1000 (- org-lowest-priority priority)))
+        (pri-current (org-get-priority (thing-at-point 'line t))))
+    (if (= pri-value pri-current)
+        subtree-end
+      nil)))
+(defun air-org-skip-subtree-if-habit ()
+  "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t))))
+    (if (string= (org-entry-get nil "STYLE") "habit")
+        subtree-end
+      nil)))
+
 ;; Include agenda archive files when searching for things
 (setq org-agenda-text-search-extra-files (quote (agenda-archives)))
 
