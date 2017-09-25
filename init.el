52190ee1b5a826b03a556d5178369715c484a8c0
Robert Irelan <rmirelan@google.com>
2017-09-25 10:46:32 -0700

    Remove obsolete Ido configuration.

--- init.el
+++ init.el
@@ -707,24 +707,15 @@ to get the latest version of the file, then make the change again.")
 (setq org-refile-targets (quote ((nil :maxlevel . 9)
                                  (org-agenda-files :maxlevel . 9))))
 
-; Use full outline paths for refile targets - we file directly with IDO
+; Use full outline paths for refile targets - we file directly with Ivy
 (setq org-refile-use-outline-path t)
 
-; Targets complete directly with IDO
+; Targets complete directly with Ivy
 (setq org-outline-path-complete-in-steps nil)
 
 ; Allow refile to create parent tasks with confirmation
 (setq org-refile-allow-creating-parent-nodes (quote confirm))
 
-;; ; Use IDO for both buffer and file completion and ido-everywhere to t
-;; (setq org-completion-use-ido t)
-;; (setq ido-everywhere t)
-;; (setq ido-max-directory-size 100000)
-;; (ido-mode (quote both))
-;; ; Use the current window when visiting files and buffers with ido
-;; (setq ido-default-file-method 'selected-window)
-;; (setq ido-default-buffer-method 'selected-window)
-; Use the current window for indirect buffer display
 (setq org-indirect-buffer-display 'current-window)
 
 ;;;; Refile settings
