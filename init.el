435bbc4e01123af46e4a9aa860a31601369e629a
Robert Irelan <rmirelan@google.com>
2017-12-05 10:24:36 -0800

    Add air-org-set-tags in order to add and remove tags incrementally

--- init.el
+++ init.el
@@ -853,6 +853,58 @@ to get the latest version of the file, then make the change again.")
   :init
   (setq org-special-ctrl-a/e t))
 
+;;;; Make tag selection more intuitive
+;;;; See https://blog.aaronbieber.com/2016/03/05/playing-tag-in-org-mode.html
+(defun air--org-swap-tags (tags)
+  "Replace any tags on the current headline with TAGS.
+
+The assumption is that TAGS will be a string conforming to Org Mode's
+tag format specifications, or nil to remove all tags."
+  (let ((old-tags (org-get-tags-string))
+        (tags (if tags
+                  (concat " " tags)
+                "")))
+    (save-excursion
+      (beginning-of-line)
+      (re-search-forward
+       (concat "[ \t]*" (regexp-quote old-tags) "[ \t]*$")
+       (line-end-position) t)
+      (replace-match tags)
+      (org-set-tags t))))
+(defun air-org-set-tags (tag)
+  "Add TAG if it is not in the list of tags, remove it otherwise.
+
+TAG is chosen interactively from the global tags completion table."
+  (interactive
+   (list (let ((org-last-tags-completion-table
+                (if (derived-mode-p 'org-mode)
+                    (org-uniquify
+                     (delq nil (append (org-get-buffer-tags)
+                                       (org-global-tags-completion-table))))
+                  (org-global-tags-completion-table))))
+           (org-icompleting-read
+            "Tag: " 'org-tags-completion-function nil nil nil
+            'org-tags-history))))
+  (let* ((cur-list (org-get-tags))
+         (new-tags (mapconcat 'identity
+                              (if (member tag cur-list)
+                                  (delete tag cur-list)
+                                (append cur-list (list tag)))
+                              ":"))
+         (new (if (> (length new-tags) 1) (concat " :" new-tags ":")
+                nil)))
+    (air--org-swap-tags new)))
+(defun air-org-set-tags-ctrl-c-ctrl-c-hook ()
+  (let* ((context (org-element-context))
+         (type (org-element-type context)))
+    (if (or (eq type 'headline)
+            (eq type 'inlinetask))
+        (save-excursion (goto-char (org-element-property :begin context))
+                        (call-interactively #'air-org-set-tags)
+                        t)
+      nil)))
+(add-hook 'org-ctrl-c-ctrl-c-hook #'air-org-set-tags-ctrl-c-ctrl-c-hook)
+(org-defkey org-mode-map (kbd "C-c C-q") #'air-org-set-tags)
 
 ;;; Org-gcal
 (use-package org-gcal
