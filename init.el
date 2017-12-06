f1960f0bd6e3baf8912d7d28bad9df1d8009dc31
Robert Irelan <rmirelan@google.com>
2017-12-05 18:32:30 -0800

    Define org-agenda command to list archivable projects

--- init.el
+++ init.el
@@ -1295,6 +1295,36 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
         subtree-end
       nil)))
 
+;;; List tasks that should be archived
+;;; http://doc.norang.ca/org-mode.html#Archiving
+(add-to-list 'org-agenda-custom-commands
+             '("A" "Archivable tasks" tags "-REFILE"
+               ((org-agenda-skip-function #'bh/skip-non-archivable-tasks)
+                (org-tags-match-list-sublevels nil)
+                (org-agenda-archives-mode nil))))
+(defun bh/skip-non-archivable-tasks ()
+  "Skip trees that are not available for archiving"
+  (save-restriction
+    (widen)
+    ;; Consider only tasks with done todo headings as archivable candidates
+    (let ((next-headline (save-excursion (or (outline-next-heading) (point-max))))
+          (subtree-end (save-excursion (org-end-of-subtree t))))
+      (if (member (org-get-todo-state) org-todo-keywords-1)
+          (if (member (org-get-todo-state) org-done-keywords)
+              (let* ((daynr (string-to-int (format-time-string "%d" (current-time))))
+                     (a-month-ago (* 60 60 24 (+ daynr 1)))
+                     (last-month (format-time-string "%Y-%m-" (time-subtract (current-time) (seconds-to-time a-month-ago))))
+                     (this-month (format-time-string "%Y-%m-" (current-time)))
+                     (subtree-is-current (save-excursion
+                                           (forward-line 1)
+                                           (and (< (point) subtree-end)
+                                                (re-search-forward (concat last-month "\\|" this-month) subtree-end t)))))
+                (if subtree-is-current
+                    subtree-end ; Has a date in this month or last month, skip it
+                  nil))  ; available to archive
+            (or subtree-end (point-max)))
+        next-headline))))
+
 ;; Include agenda archive files when searching for things
 (setq org-agenda-text-search-extra-files (quote (agenda-archives)))
 
