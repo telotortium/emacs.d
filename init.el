83f260d17116e385dc565f23344177c4e1270e29
Robert Irelan <rmirelan@google.com>
2019-05-18 20:47:38 -0700

    Ensure Emacs server name is server
    I hope this fixes `with-editor` occasionally corrupting the server
    name and causing my emacsclient to not be able to find the server.

--- init.el
+++ init.el
@@ -93,6 +93,7 @@ http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/."
 (use-package server
   :ensure nil
   :config
+  (c-setq server-name "server")
   (c-setq server-socket-dir "~/.emacs.d/server")
   (c-setq server-use-tcp t)
   (if (server-running-p)
